680666e5b2ed8cc6c1bf3361bc18ecbf7471f646
c-blaq <babayejuadesina@gmail.com>
2024-05-04 18:22:29 +0100

    style: livescores page

--- src/app/livescores/page.tsx
+++ src/app/livescores/page.tsx
@@ -4,7 +4,26 @@ import React from "react";
 const Livescores = () => {
   return (
     <div className="pt-4">
-      <section className="px-5">
+      <div className="text-dark-200">
+        <div className="border-b border-b-[#252525] p-2">
+          <div className="flex overflow-auto gap-3 text-sm">
+            <div className="border border-primary/50 text-white/90 rounded-full py-2 px-4">
+              Soccer
+            </div>
+            <div className="p-2">Basketball</div>
+            <div className="p-2">Cricket</div>
+            <div className="p-2">Volleyball</div>
+            <div className="p-2">Tennis</div>
+            <div className="p-2">Baseball</div>
+          </div>
+        </div>
+
+        <div className="border-b border-b-[#252525] p-2 text-center">
+          Calendar here
+        </div>
+      </div>
+
+      <section className="px-5 mt-4">
         <div className="bg-dark rounded-2xl mb-5 pt-3 pb-5 px-1">
           <div className="bg-black flex justify-between items-center gap-3 p-3 text-dark-200 font-semibold rounded-full text-xs">
             <h2>Premier League</h2>
