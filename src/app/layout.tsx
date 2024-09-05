f37e3a9590522e38646d4d3ac060a48bac8f19fd
Nkem Benjamin <benjaminnkemfrancis@gmail.com>
2024-09-05 03:18:31 +0100

    fix: width snapping

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="dark">
-      <body className={twMerge(sbt.className, "bg-black text-white max-w-2xl mx-auto")}>
-        <Providers>{children}</Providers>
+      <body className={twMerge(sbt.className, "bg-black text-white ")}>
+        <div className="w-[42rem] mx-auto">
+          <Providers>{children}</Providers>
+        </div>
       </body>
     </html>
   );
